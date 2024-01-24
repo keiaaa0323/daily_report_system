2bd2d7e201777d9afaaf40bcb69bb648e56fd36f
keisu <keisuke1503230920@gmail.com>
2024-01-25 07:12:58 +0900

    ログアウトアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -246,4 +246,22 @@ public class EmployeeAction extends ActionBase {
             }
         }
 
+        /**
+         * ログアウト処理を行う
+         * @throws ServletException
+         * @throws IOException
+         */
+        public void logout() throws ServletException, IOException {
+
+            //セッションからログイン従業員のパラメータを削除
+            removeSessionScope(AttributeConst.LOGIN_EMP);
+
+            //セッションにログアウト時のフラッシュメッセージを追加
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+            //ログイン画面にリダイレクト
+            redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
+        }
+
 }
